bfc96e08605d56fc86326f101db4aaa3d3a3ecfd
codewithash-hub <402308182@my.richfield.ac.za>
2025-03-10 12:48:24 +0200

    create a contact info model calss

--- CONTACT_BOOK/contactBook/contact_book/models.py
+++ CONTACT_BOOK/contactBook/contact_book/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact_Information(models.Model):
+    Store_name = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    email = models.EmailField(max_length=255)
+    address = models.CharField(max_length=255)
